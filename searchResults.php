e1596e346fafa9981bd1be58880b75a54d2a78ea
unknown <aknoxlor@gmail.com>
2014-12-08 17:59:30 -0500

    Formatted search results

--- searchResults.php
+++ searchResults.php
@@ -7,17 +7,17 @@
 <?php
 	if($_POST["searchText"] != null)
 	{
-		$search = "%" . $_POST["searchText"] . "%";
-		echo $search . "<br><br>";
-		$query; $fetch;
+		$search = "% " . $_POST["searchText"] . " %"; //No partial-word matches
+		//echo $search . "<br><br>";
+		$query; $fetch; $syntax;
 		putenv("ORACLE_HOME=/usr/local/libexec/oracle11g-client");
 		$conn = ocilogon("lorelle", $_POST["dbPass"], "oracle.cise.ufl.edu:1521/orcl"); //Replace USERNAME with your own. Temporarily pulling passwords from form
 		switch($_POST["searchType"])
 		{
-			case "song": $query = ociparse($conn, "SELECT * FROM dballard.songs WHERE title LIKE :search_bv"); break;
-			case "artist": $query = ociparse($conn, "SELECT * FROM dballard.artists WHERE name LIKE :search_bv"); break;
-			case "album": $query = ociparse($conn, "SELECT * FROM dballard.albums WHERE release LIKE :search_bv"); break;
-			default: $query = ociparse($conn, "SELECT * FROM dballard.songs WHERE :syntax_bv LIKE :search_bv"); break;
+			case "song": $query = ociparse($conn, "SELECT * FROM dballard.songs WHERE title LIKE :search_bv ORDER BY title"); $syntax = "TITLE"; break;
+			case "artist": $query = ociparse($conn, "SELECT * FROM dballard.artists WHERE name LIKE :search_bv"); $syntax = "NAME"; break;
+			case "album": $query = ociparse($conn, "SELECT * FROM dballard.albums WHERE release LIKE :search_bv"); $syntax = "RELEASE"; break;
+			default: $query = ociparse($conn, "SELECT * FROM dballard.songs WHERE :syntax_bv LIKE :search_bv");  $syntax = "TITLE"; break;
 		}
 		//oci_bind_by_name($query, ":db_bv", $dB);
 		//oci_bind_by_name($query, ":syntax_bv", $syntax);
@@ -29,13 +29,10 @@
 		
 		//var_dump($fetch);
 		
-		foreach($fetch as $row)
+		for($row = 0; $row < count($fetch[$syntax]); $row++)
 		{
-			foreach($row as $column)
-			{
-				echo " " . $column . " ";
-			}
-			echo "<br>";
+			//echo "Name: " . $fetch[$syntax][$row] . "<br>Duration: " . $fetch["DURATION"][$row] . "<br><br>";
+			echo "Name: " . $fetch[$syntax][$row] . "<br>";
 		}
 	}
 ?>
