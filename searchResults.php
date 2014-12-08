ebc1ae3d6e787c21ee54270380043085cfdbbc31
unknown <aknoxlor@gmail.com>
2014-12-08 17:41:02 -0500

    Search actually works this time

--- searchResults.php
+++ searchResults.php
@@ -7,13 +7,14 @@
 <?php
 	if($_POST["searchText"] != null)
 	{
-		$search = "'%" + $_POST["searchText"] + "%'";
-		$query = null;
+		$search = "%" . $_POST["searchText"] . "%";
+		echo $search . "<br><br>";
+		$query; $fetch;
 		putenv("ORACLE_HOME=/usr/local/libexec/oracle11g-client");
 		$conn = ocilogon("lorelle", $_POST["dbPass"], "oracle.cise.ufl.edu:1521/orcl"); //Replace USERNAME with your own. Temporarily pulling passwords from form
 		switch($_POST["searchType"])
 		{
-			case "song": $query = ociparse($conn, "SELECT title FROM dballard.songs WHERE title LIKE :search_bv"); break;
+			case "song": $query = ociparse($conn, "SELECT * FROM dballard.songs WHERE title LIKE :search_bv"); break;
 			case "artist": $query = ociparse($conn, "SELECT * FROM dballard.artists WHERE name LIKE :search_bv"); break;
 			case "album": $query = ociparse($conn, "SELECT * FROM dballard.albums WHERE release LIKE :search_bv"); break;
 			default: $query = ociparse($conn, "SELECT * FROM dballard.songs WHERE :syntax_bv LIKE :search_bv"); break;
@@ -26,13 +27,14 @@
 		oci_free_statement($query);
 		ocilogoff($conn);
 		
-		for($row = 0; $row < count($fetch); $row++)
+		//var_dump($fetch);
+		
+		foreach($fetch as $row)
 		{
-			$dataRow = $fetch[$row];
-			for($column = 0; $column < count($dataRow); $column++)
+			foreach($row as $column)
 			{
-				echo " " + $dataRow[$column] + " ";
-			
+				echo " " . $column . " ";
+			}
 			echo "<br>";
 		}
 	}
