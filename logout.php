7fa3084b798de41f0678f57bf32ccabeb80878a0
dballard23 <dballard23@ufl.edu>
2014-12-08 21:00:20 -0500

    Send user here to logout

---
+++ logout.php
@@ -0,0 +1,13 @@
+#!/usr/local/bin/php
+
+<html><body>
+
+<?php
+session_start();
+session_destroy();
+print "Logout Successful";
+?>
+
+<br>Will redirect back to the home page in 3 seconds.<br>
+<meta http-equiv="refresh" content="3;url=home.php" />
+</body></html>
