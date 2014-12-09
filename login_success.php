05bb1e82bfd845a7d385124f9a1a269eadf921e6
dballard23 <dballard23@ufl.edu>
2014-12-08 20:58:49 -0500

    Used by logincheck.php
    Automatically redirects to "home.php", replace url at line 15 to redirect to something else.

---
+++ login_success.php
@@ -0,0 +1,17 @@
+#!/usr/local/bin/php
+
+<?php
+session_start();
+if(!session_is_registered(username)){
+  header("location:login.php");
+}
+?>
+
+<html>
+<body>
+<h1>Welcome!</h1>
+<b>Login Successful!</b>
+<br>Will redirect back to the home page in 3 seconds.<br>
+<meta http-equiv="refresh" content="3;url=home.php" />
+</body>
+</html>
