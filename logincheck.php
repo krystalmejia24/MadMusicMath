3c2e7055bcb2000caae60221b4fd408615c8acf5
dballard23 <dballard23@ufl.edu>
2014-12-08 20:57:18 -0500

    Used by login.php

---
+++ logincheck.php
@@ -0,0 +1,33 @@
+#!/usr/local/bin/php
+
+<?php
+
+putenv("ORACLE_HOME=/usr/local/libexec/oracle11g-client");
+$conn = ocilogon("dballard", "Aa125897", "oracle.cise.ufl.edu:1521/orcl");
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$search = ociparse($conn, "SELECT username, password FROM users WHERE username='$username' and password='$password'");
+ociexecute($search);
+
+if($value=oci_fetch_row($search)){
+  session_start();
+  session_register("username");
+  session_register("password");
+  $_SESSION["username"] = $username;
+  $_SESSION["password"] = $password;
+  header("location:login_success.php");
+}
+else {
+  echo "<html><body>";
+  echo "<b>Login failed.<b><br>";
+  echo "<a href=\"login.php\">Try Again?</a>";
+  echo "</body></html>";
+}
+
+oci_free_statement($search);
+ocilogoff($conn);
+oci_close($conn);
+
+?>
